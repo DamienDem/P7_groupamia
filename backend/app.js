9b8abfd4620eb0eed287e65fbde077523546c0db
DamienDem <damien.demolin@gmail.com>
2022-01-12 16:55:22 +0100

    Optimisation du serveur

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 
 const app = express();
-const port = 3000
+
 
 app.get('/', (req,res) => res.send('Hello express !'))
 
-app.listen(port, () => console.log(`Notre application est démarrée sur: http://localhost:${port}`));
-
-
+app.use(express.json())
+module.exports = app;
