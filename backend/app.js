a38acdc1a6cdd6ad00d23721ea01548078236871
DamienDem <damien.demolin@gmail.com>
2022-01-12 17:45:15 +0100

    initialisation de sequelize

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const Sequelize = require('sequelize')
 
 const app = express();
 
+const sequelize = new Sequelize('groupamia', 'root', '', {
+    host: 'localhost',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: false
+  })
+    
 
 app.get('/', (req,res) => res.send('Hello express !'))
 
