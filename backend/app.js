413eb7ad74b87306ad74d5af543fdf92be2da65a
DamienDem <damien.demolin@gmail.com>
2022-01-17 11:44:49 +0100

    Création de la fonction login

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const res = require('express/lib/response');
 const { Sequelize, DataTypes } = require('sequelize')
-const UserModel = require('./models/user')
+const UserModel = require('./models/user');
+const user = require('./models/user');
 
 const app = express();
 
@@ -28,11 +29,24 @@ const User = UserModel(sequelize, DataTypes)
 sequelize.sync({force:true})
 .then(_=> console.log('la BDD a bien été synchronisée'))
 
+
 app.post('/signup', (req,res) => {
   User.create(req.body)
   .then(user => {
     const message = `L'utilisateur ${req.body.userName} a bien été créé .`
     res.json({message, data: user})
+  })
+})
+
+app.post('/login', (req, res) => {
+
+  User.findOne({ where: { email: req.body.email } }).then(user => {
+    if(!user) {
+      const message = `L'utilisateur demandé n'existe pas.`
+      return res.status(404).json({ message })
+    }
+    const message = `L'utilisateur a été connecté avec succès`;
+    return res.json({ message, data: user })
   })
 })
 
