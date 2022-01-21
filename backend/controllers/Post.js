746cb33adaa9035a1cf03ef705b3118b49dbfafb
DamienDem <damien.demolin@gmail.com>
2022-01-21 10:51:01 +0100

    Création de la fonction getAllPosts

--- backend/controllers/Post.js
+++ backend/controllers/Post.js
@@ -18,18 +18,29 @@ exports.createPost = (req, res) => {
                 const message = `L'utilisateur n'existe pas`
                 res.status(404).json({ message })
             }
-            console.log(user)
             Post.create({
               userId : req.params.id,
               title  : title,
               content: content,
               like  : 0
-            })
-        
+            })  
             const message = `La publication est postée`;
             return res.json({ message, data: user, })
         })
-      }
+      };
+
+exports.getAllPosts = (req,res) => {
+  
+  Post.findAll()
+  .then(posts => {
+    const message = `la liste des publications a bien été récupérée . `
+    res.json({ message, data : posts})
+  })
+  .catch(err => {
+    const message = `La liste des publications n'a pas pu être récupérée. Réessayer dans quelques instants.`
+    res.status(500).json({message, data: err})
+  })
+}
        
         
           
--- backend/routes/Post.js
+++ backend/routes/Post.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const postCtrl = require('../controllers/Post');
 
 router.post('/post/:id', postCtrl.createPost);
+router.get('/posts', postCtrl.getAllPosts )
 
 module.exports = router;
