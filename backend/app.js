f220cbfe97665ae25314d88b68d96127ce731261
DamienDem <damien.demolin@gmail.com>
2022-02-05 18:37:40 +0100

    ajout su style + modification de la fonction updateUser (back)

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
-const userRoutes = require('./routes/User');
-const postRoutes = require('./routes/Post');
-const likeRoutes = require('./routes/Like');
-const commentRoutes = require('./routes/Comment');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const userRoutes = require("./routes/User");
+const postRoutes = require("./routes/Post");
+const likeRoutes = require("./routes/Like");
+const commentRoutes = require("./routes/Comment");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 
-
 app
-.use(express.json())
-.use('/images', express.static(path.join(__dirname, 'images')))
-.use(morgan('dev'))
-.use('/', userRoutes)
-.use('/', postRoutes)
-.use('/', likeRoutes)
-.use('/', commentRoutes)
+  .use(express.json())
+  .use("/images", express.static(path.join(__dirname, "images")))
+  .use(morgan("dev"))
+  .use("/", userRoutes)
+  .use("/", postRoutes)
+  .use("/", likeRoutes)
+  .use("/", commentRoutes);
 
 module.exports = app;
--- backend/controllers/User.js
+++ backend/controllers/User.js
@@ -1,121 +1,128 @@
-const express = require('express');
-const { User } = require('../db/sequelize');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
+const express = require("express");
+const { User } = require("../db/sequelize");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
-exports.signup = (req,res) => {
-  User.findOne({ where: { email: req.body.email } })
-  .then(user => {
-    if(!user) {
-      bcrypt.hash(req.body.password, 10)
-      .then(hash => {
-        let newUser = User.create({
+exports.signup = (req, res) => {
+  User.findOne({ where: { email: req.body.email } }).then((user) => {
+    if (!user) {
+      bcrypt
+        .hash(req.body.password, 10)
+        .then((hash) => {
+          let newUser = User.create({
             name: req.body.name,
-            firstName:req.body.firstName,
+            firstName: req.body.firstName,
             email: req.body.email,
             password: hash,
             picture: `http://localhost:3000/images/default_profil_picture`,
-            isAdmin: false
+            isAdmin: false,
+          });
         })
-      })
-      .then((newUser) => { 
-        const message = `L'utilisateur ${req.body.name} ${req.body.firstName} a bien été créé .`
-        res.json({message, data: newUser})
-      })
-      .catch((err => {
-        const message = `impossible de créer l'utilisateur:"${req.body.name} ${req.body.firstName}" `
-        res.status(400).json({ message, err})
-      }))
+        .then((newUser) => {
+          const message = `L'utilisateur ${req.body.name} ${req.body.firstName} a bien été créé .`;
+          res.json({ message, data: newUser });
+        })
+        .catch((err) => {
+          const message = `impossible de créer l'utilisateur:"${req.body.name} ${req.body.firstName}" `;
+          res.status(400).json({ message, err });
+        });
     } else {
-      const message = `L'utilisateur avec l'email:"${req.body.email}" existe déja. `
-      return res.status(400).json({ message})
+      const message = `L'utilisateur avec l'email:"${req.body.email}" existe déja. `;
+      return res.status(400).json({ message });
     }
-  })
+  });
 };
 
 exports.login = (req, res) => {
-  User.findOne({ where: { email: req.body.email } }).then(user => {
-    if(!user) {
-      const message = `L'utilisateur demandé n'existe pas.`
-      return res.status(404).json({ message })
+  User.findOne({ where: { email: req.body.email } }).then((user) => {
+    if (!user) {
+      const message = `L'utilisateur demandé n'existe pas.`;
+      return res.status(404).json({ message });
     }
-    bcrypt.compare(req.body.password, user.password)
-    .then(valid => {
-      if(!valid) {
-        const message = 'Mot de passe est incorrect ! '
-        return res.status(401).json({ message })
-      }
-      const token = jwt.sign(
-        { userId: user.id },
-        `Mon_token_secret`,
-        { expiresIn: '24h' }
-      )
-      const message = `L'utilisateur a été connecté avec succès`;
-      return res.json({ message, data: user, token })
-    })
-    .catch(err => {
-      const message = `Impossible de se connecter, veuillez réessayer ultérieurement. `
-      res.status(500).json({message ,err})
-    })
-  })
+    bcrypt
+      .compare(req.body.password, user.password)
+      .then((valid) => {
+        if (!valid) {
+          const message = "Mot de passe est incorrect ! ";
+          return res.status(401).json({ message });
+        }
+        const token = jwt.sign({ userId: user.id }, `Mon_token_secret`, {
+          expiresIn: "24h",
+        });
+        const message = `L'utilisateur a été connecté avec succès`;
+        return res.json({ message, data: user, token });
+      })
+      .catch((err) => {
+        const message = `Impossible de se connecter, veuillez réessayer ultérieurement. `;
+        res.status(500).json({ message, err });
+      });
+  });
 };
 
-exports.getAllUsers = (req,res) => {
+exports.getAllUsers = (req, res) => {
   User.findAll()
-  .then(users => {
-    const message = `la liste des utilisateurs a bien été récupérée . `
-    res.json({ message, data : users })
-  })
-  .catch(err => {
-    const message = `La liste des utilisateurs n'a pas pu être récupérée. Réessayer dans quelques instants.`
-    res.status(500).json({message, data: err})
-})
+    .then((users) => {
+      const message = `la liste des utilisateurs a bien été récupérée . `;
+      res.json({ message, data: users });
+    })
+    .catch((err) => {
+      const message = `La liste des utilisateurs n'a pas pu être récupérée. Réessayer dans quelques instants.`;
+      res.status(500).json({ message, data: err });
+    });
 };
 
-exports.getOneUser = (req,res) => {
+exports.getOneUser = (req, res) => {
   User.findOne({
-    where: { id: req.params.id}
-  })
-  .then(user => {
-    if(user === null ){
-      const message =  `L'utilisateur n'existe pas `
-      return res.status(400).json({ message })
+    where: { id: req.params.id },
+  }).then((user) => {
+    if (user === null) {
+      const message = `L'utilisateur n'existe pas `;
+      return res.status(400).json({ message });
     }
-    const message = `L'utilisateur a bien été trouvé `
-    res.json({ message, data : user })
-  })
+    const message = `L'utilisateur a bien été trouvé `;
+    res.json({ message, data: user });
+  });
 };
 
 exports.updateProfil = (req, res) => {
-  const userObject = req.file ?
-    {
-      ...JSON.parse(req.body.user),
-      picture: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
+  const userObject = req.file
+    ? {
+        ...JSON.parse(req.body.user),
+        picture: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
 
-  User.update(userObject,{
-    where: { id: req.params.id }
-  })
-  .then(userModify => {
-      const message = `L'utilisateur ${userModify} a bien été modifié`
-      res.json({ message, data: userModify})
-    }) 
-    .catch(err => {
-      const message = `Impossible de modifier le profil, veuillez réessayer ultérieurement. `
-      res.status(500).json({message ,err})
-    }) 
-}
-
-exports.deleteUser = (req,res) => {
   User.findByPk(req.params.id)
-  .then(user => {
-    User.destroy({ where: {id:user.id} })
-    const message = `L'utilisateur ${user.userName} a bien été supprimé .`
-    res.json({message, data: user})
-  })
-  .catch(err => {
-    const message = `Impossible de supprimer le profil, veuillez réessayer ultérieurement. `
-    res.status(500).json({message ,err})
-  })
+    .then((user) => {
+      if (user === null) {
+        const message = "L'utilisateur demandé n'existe pas .";
+        return res.status(404).json({ message });
+      }
+      const message = `L'utilisateur ${user.name} ${user.firstName} a bien été modifié`;
+      res.json({ message, data: user });
+    })
+    .then((_) => {
+      User.update(userObject,{
+        where: { id: req.params.id },
+      }).catch((err) => {
+        const message = `Impossible de modifier le profil, veuillez réessayer ultérieurement. `;
+        res.status(500).json({ message, err });
+      });
+    });
+};
+
+exports.deleteUser = (req, res) => {
+  User.findByPk(req.params.id)
+    .then((user) => {
+      User.destroy({ where: { id: user.id } });
+      const message = `L'utilisateur ${user.userName} a bien été supprimé .`;
+      res.json({ message, data: user });
+    })
+    .catch((err) => {
+      const message = `Impossible de supprimer le profil, veuillez réessayer ultérieurement. `;
+      res.status(500).json({ message, err });
+    });
 };
